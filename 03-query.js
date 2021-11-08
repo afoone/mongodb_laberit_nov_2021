131546da14729239040f44311e99d58f1591de7e
Alfonso Tienda <afoone@hotmail.com>
2021-11-08 10:20:26 +0100

    service

--- 03-query.js
+++ 03-query.js
@@ -1,10 +1,13 @@
+let service = ['OBSTETRICIA', 'URGENCIAS', 'NEONATOLOGÍA', 'PEDIATRÍA', 'ONCOLOGÍA']
+
 for (let i = 0; i<100; i++) {
     db.patients.insertOne(
         {
             name: 'patient'+i,
             age: Math.floor(
                 Math.random() * 100
-            )
+            ),
+            service: service[Math.floor(Math.random()*service.length)]
         }
     )
 }
