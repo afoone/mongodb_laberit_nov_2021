1ba0959cdddd9fb72d566eb92cbb7fb6338442c2
Alfonso Tienda <afoone@hotmail.com>
2021-11-10 13:50:48 +0100

    indexes

--- 04-indexes.js
+++ 04-indexes.js
@@ -1,5 +1,5 @@
 for (let i =  0; i< 1000000; i++) {
-    db.insertOne(
+    db.users.insertOne(
         {
             "i": i,
             "username": "user_"+i,
@@ -9,3 +9,81 @@ for (let i =  0; i< 1000000; i++) {
     )
 }
 
+db.users.find(
+    {
+        username: 'user_134'
+    }
+).explain('executionStats')
+
+db.users.createIndex(
+    {
+        username: 1
+    }
+)
+
+db.users.find().sort(
+    {
+        age: 1,
+        username: 1
+    }
+)
+
+db.users.createIndex(
+    {
+        age: 1,
+        username: 1
+    }
+)
+
+db.users.find().sort( { age: 1, username: 1 }).explain(
+    'executionStats'
+)
+
+db.users.find().sort( {  username: 1 , age: 1}).explain(
+    'executionStats'
+)
+
+
+db.users.getIndexes()
+
+db.users.find(
+    { 
+        age: 34
+    }
+).explain('executionStats')
+
+db.users.find(
+    { 
+        age: 34,
+        username: {
+            $regex: /user_12/
+        }
+    }
+).explain('executionStats')
+
+
+db.users.find(
+    { 
+        age: 34,
+        username: {
+            $regex: /^user_12/
+        }
+    }
+).explain('executionStats')
+
+
+db.users.dropIndex('username_1')
+
+db.users.createIndex(
+    {
+        username: 1
+    },
+    {
+        unique: true, 
+        partialFilterExpression: {
+            username: {
+                $exists: true
+            }
+        }
+    }
+)
