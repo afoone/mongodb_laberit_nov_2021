56311f0addc4c870aaf65083a0b15f15a7ccd628
Alfonso Tienda <afoone@hotmail.com>
2021-11-12 12:12:51 +0100

    datos almacen

---
+++ data/almacen.js
@@ -0,0 +1,94 @@
+[
+    {
+        "name": "producto 1",
+        "price" : 34,
+        "type": "TIPO1",
+        "stock": [
+            {
+                "place": "almacen 1",
+                "value": 2
+            },
+            {
+                "place": "almacen 2",
+                "value": 4
+            },
+            {
+                "place": "almacen 3",
+                "value": 8
+            },
+            {
+                "place": "almacen 4",
+                "value": 0
+            }
+        ]
+    },
+    {
+        "name": "producto 2",
+        "price" : 25,
+        "type": "TIPO1",
+        "stock": [
+            {
+                "place": "almacen 1",
+                "value": 6
+            },
+            {
+                "place": "almacen 2",
+                "value": 4
+            },
+            {
+                "place": "almacen 3",
+                "value": 4
+            },
+            {
+                "place": "almacen 4",
+                "value": 0
+            }
+        ]
+    },
+    {
+        "name": "producto 3",
+        "price" : 6,
+        "type": "TIPO2",
+        "stock": [
+            {
+                "place": "almacen 1",
+                "value": 2
+            },
+            {
+                "place": "almacen 2",
+                "value": 7
+            },
+            {
+                "place": "almacen 3",
+                "value": 8
+            },
+            {
+                "place": "almacen 4",
+                "value": 6
+            }
+        ]
+    },
+    {
+        "name": "producto 4",
+        "price" : 3,
+        "type": "TIPO2",
+        "stock": [
+            {
+                "place": "almacen 1",
+                "value": 0
+            },
+            {
+                "place": "almacen 2",
+                "value": 4
+            },
+            {
+                "place": "almacen 3",
+                "value": 0
+            },
+            {
+                "place": "almacen 4",
+                "value": 0
+            }
+        ]
+    }
+]
